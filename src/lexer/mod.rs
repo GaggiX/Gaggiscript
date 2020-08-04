7db8873404f4bedd331b36209384d9c21bc46946
GaggiX <fedegaggix@gmail.com>
2020-08-04 16:00:30 +0200

    lexer extended

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -11,6 +11,14 @@ pub enum Token {
     //operators
     EqualSign,
     PlusSign,
+    MinusSign,
+    BangSign,
+    AsteriskSign,
+    SlashSign,
+    LT,
+    GT,
+    EQ,
+    NotEQ,
     
     //delimiters
     Comma,
@@ -22,7 +30,12 @@ pub enum Token {
 
     //keyword
     Function,
-    Let
+    Let,
+    True,
+    False,
+    If,
+    Else,
+    Return
 }
 
 #[derive(Clone, Copy)]
@@ -82,8 +95,14 @@ impl<'b> Lexer<'b> {
         self.skip_whitespace();
 
         match self.ch as char {
-            '='  => token = Token::EqualSign,
+            '='  => token = if self.peek_char('=') {Token::EQ} else {Token::EqualSign},
+            '!'  => token = if self.peek_char('=') {Token::NotEQ} else {Token::BangSign},
             '+'  => token = Token::PlusSign,
+            '-'  => token = Token::MinusSign,
+            '*'  => token = Token::AsteriskSign,
+            '/'  => token = Token::SlashSign,
+            '<'  => token = Token::LT,
+            '>'  => token = Token::GT,
             ','  => token = Token::Comma,
             ';'  => token = Token::Semicolon,
             '('  => token = Token::Lparen,
@@ -93,7 +112,7 @@ impl<'b> Lexer<'b> {
             '\0' => token = Token::EOF,
             _    => {
                     if is_letter(self.ch as char) {
-                        return self.read_identifier();
+                        return self.read_literal();
                     } else if is_digit(self.ch as char) {
                         return self.read_number();
                     } else {
@@ -107,23 +126,41 @@ impl<'b> Lexer<'b> {
     }
 
     fn skip_whitespace(&mut self) {
-        while self.ch as char == ' ' || self.ch as char == '\n' || self.ch as char == '\r' {
+        while self.ch as char == ' ' || self.ch as char == '\n' || self.ch as char == '\r' || self.ch as char == '\t' {
             self.read_char();
         }
     }
 
-    fn read_identifier(&mut self) -> Token {
+    fn peek_char(&mut self, chr: char) -> bool {
+        if if self.read_position >= self.code.len() {
+            '\0'
+        } else {
+            self.code.as_bytes()[self.read_position] as char
+        } == chr {
+            self.read_char();
+            true
+        } else {
+            false
+        }
+    }
+
+    fn read_literal(&mut self) -> Token {
         let position = self.position;
         while is_letter(self.ch as char) {
             self.read_char();
         }
 
         match &self.code[position..self.position] {
-            "fn"  => Token::Function,
-            "let" => Token::Let,
-            _     => Token::Identifier(
-                        String::from(&self.code[position..self.position])
-                     )
+            "fn"     => Token::Function,
+            "let"    => Token::Let,
+            "true"   => Token::True,
+            "false"  => Token::False,
+            "if"     => Token::If,
+            "else"   => Token::Else,
+            "return" => Token::Return,
+            _        => Token::Identifier(
+                            String::from(&self.code[position..self.position])
+                        )
         }
     }
 
